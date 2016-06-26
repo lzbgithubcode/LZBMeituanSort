13148591c598a71629676167f91a8f5d140d560d
apple <apple@platonademacbook-pro.local>
2016-06-26 23:28:56 +0800

    排序

--- LZBMeituanSort/LZBMeituanSort/ViewController.m
+++ LZBMeituanSort/LZBMeituanSort/ViewController.m
@@ -17,7 +17,7 @@ static NSString *LZBFoodTableViewCellID = @"LZBFoodTableViewCellID";
 
 @property (nonatomic, strong) UITableView *tableView;
 
-@property (nonatomic, strong) NSMutableArray *foodModes;
+@property (nonatomic, strong) NSMutableArray <LZBBaseModel *>*foodModes;
 
 @property (nonatomic, strong) LZBMenuListView  *listView;
 
@@ -70,12 +70,14 @@ static NSString *LZBFoodTableViewCellID = @"LZBFoodTableViewCellID";
     chooseView.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, height);
     
     __weak typeof(chooseView)  weakChooseView  = chooseView;
+    __weak typeof (self) weakSelf = self;
     [chooseView setClickChooseView:^(LZBChooseViewType type) {
        
-        [self.listView showPoint:CGPointMake([UIScreen mainScreen].bounds.size.width-10-70, height) WithInView:self.view];
+        [weakSelf.listView showPoint:CGPointMake([UIScreen mainScreen].bounds.size.width-10-70, height) WithInView:weakSelf.view];
         
-        [self.listView setMemuClick:^(LZBMenuListViewType type, NSString *text) {
+        [weakSelf.listView setMemuClick:^(LZBMenuListViewType type, NSString *text) {
             weakChooseView.titles = @[text,text,text];
+            [weakSelf sortWithType:type];
         }];
         
     }];
@@ -88,7 +90,189 @@ static NSString *LZBFoodTableViewCellID = @"LZBFoodTableViewCellID";
     return 64;
 }
 
+#pragma mark - 排序算法
 
+- (void)sortWithType:(LZBMenuListViewType) type
+{
+    switch (type) {
+        case LZBMenuListViewType_Price:
+        {
+          //冒泡排序-数组
+            [self bubblingSort];
+
+        }
+            break;
+        case LZBMenuListViewType_Mark:
+        {
+           //快速排序
+            [self fastSortLeftIndex:0 WithRightIndex:self.foodModes.count -1];
+            
+        }
+            break;
+         case LZBMenuListViewType_Auto:
+            {
+                [self fastSortWithType:LZBMenuListViewType_Auto LeftIndex:0 WithRightIndex:self.foodModes.count -1];
+            }
+            break;
+            
+        default:
+            break;
+    }
+    [self.tableView reloadData];
+}
+//冒泡排序 - 价格
+- (void)bubblingSort
+{
+    
+    //方法一:左边归位
+//    for (NSInteger i = 0; i < self.foodModes.count ; i++)
+//    {
+//        for (NSInteger j = i; j < self.foodModes.count; j++)
+//         {
+//             LZBBaseModel *model_i  = self.foodModes[i];
+//             LZBBaseModel *model_j = self.foodModes[j];
+//            
+//             if(model_i.price > model_j.price)
+//             {
+//                 [self.foodModes replaceObjectAtIndex:i withObject:model_j];
+//                 [self.foodModes replaceObjectAtIndex:j withObject:model_i];
+//             }
+//            
+//        }
+//        
+//    }
+    
+    //方法二：右边归位
+    for(NSInteger i = 0; i < self.foodModes.count;i++)
+    {
+        for(NSInteger j = 0; j < self.foodModes.count - i; j++)
+        {
+            if(j == self.foodModes.count -1) break;
+            LZBBaseModel *model_j = self.foodModes[j];
+            LZBBaseModel *model_j1 = self.foodModes[j+1];
+            if(model_j.price > model_j1.price)
+            {
+                 [self.foodModes replaceObjectAtIndex:j withObject:model_j1];
+                 [self.foodModes replaceObjectAtIndex:j+1 withObject:model_j];
+            }
+        }
+    }
+    
+    //冒泡排序是时间复杂度是N *（N - i）约N * N
+}
+
+//快速排序- 星级评分
+- (void)fastSortLeftIndex:(NSInteger)left WithRightIndex:(NSInteger)right
+{
+    NSInteger i, j;
+    if(left > right) return;
+    LZBBaseModel *tempModel = self.foodModes[left]; //基准模型
+    i = left;
+    j = right;
+    
+    while (i != j) {
+        //从右边开始找比基准模型的评分小的模型
+      
+        while (i<j && self.foodModes[j].evaluation>tempModel.evaluation) {
+            j--;
+        }
+        //从左向右找
+        while (i < j  && self.foodModes[i].evaluation <= tempModel.evaluation) {
+            i++;
+        }
+        
+        //如果左右都找到了就交换模型
+        if(i < j)
+        {
+            LZBBaseModel *model_i = self.foodModes[i];
+            LZBBaseModel *model_j = self.foodModes[j];
+             [self.foodModes replaceObjectAtIndex:i withObject:model_j];
+             [self.foodModes replaceObjectAtIndex:j withObject:model_i];
+        }
+    }
+    
+    //如果相遇了
+    self.foodModes[left] = self.foodModes[i];
+    self.foodModes[i] = tempModel;
+    
+    //第一轮结束之后，采用递归 - 二分法
+    
+    [self fastSortLeftIndex:left WithRightIndex:i-1];
+    [self fastSortLeftIndex:i+1 WithRightIndex:right];
+    return;
+    
+    //快速排序时间复杂度:N * logN - 所以我比较喜欢快速排序哦
+}
+
+//按照类型快速排序
+- (void)fastSortWithType:(LZBMenuListViewType)type LeftIndex:(NSInteger)left WithRightIndex:(NSInteger)right
+{
+    NSInteger i, j;
+    if(left > right) return;
+    LZBBaseModel *tempModel = self.foodModes[left]; //基准模型
+    i = left;
+    j = right;
+    
+    while (i != j) {
+        
+        //从右边开始找比基准模型的评分小的模型
+        if(type == LZBMenuListViewType_Auto)
+        {
+            while (i<j && self.foodModes[j].ID>tempModel.ID) {
+                j--;
+            }
+            //从左向右找
+            while (i < j  && self.foodModes[i].ID <= tempModel.ID) {
+                i++;
+            }
+        }
+        else if (type == LZBMenuListViewType_Mark)
+        {
+            while (i<j && self.foodModes[j].evaluation>tempModel.evaluation) {
+                j--;
+            }
+            //从左向右找
+            while (i < j  && self.foodModes[i].evaluation <= tempModel.evaluation) {
+                i++;
+            }
+        }
+        else
+        {
+            while (i<j && self.foodModes[j].price>tempModel.price) {
+                j--;
+            }
+            //从左向右找
+            while (i < j  && self.foodModes[i].price <= tempModel.price) {
+                i++;
+            }
+            
+        }
+
+        //如果左右都找到了就交换模型
+        if(i < j)
+        {
+            LZBBaseModel *model_i = self.foodModes[i];
+            LZBBaseModel *model_j = self.foodModes[j];
+            [self.foodModes replaceObjectAtIndex:i withObject:model_j];
+            [self.foodModes replaceObjectAtIndex:j withObject:model_i];
+        }
+    }
+    
+    //如果相遇了
+    self.foodModes[left] = self.foodModes[i];
+    self.foodModes[i] = tempModel;
+    
+    //第一轮结束之后，采用递归 - 二分法
+    [self fastSortWithType:type LeftIndex:left WithRightIndex:i-1];
+    [self fastSortWithType:type LeftIndex:i+1 WithRightIndex:right];
+
+    return;
+}
+
+
+
+
+#pragma mark - 懒加载
 
 - (LZBMenuListView *)listView
 {
